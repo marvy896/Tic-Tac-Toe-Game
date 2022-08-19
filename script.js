a9a10f9469a7cbcc7ac646489487f150f91f3b0e
marvy896 <onyex896@gmail.com>
2022-08-19 09:29:05 +0100

    The game logic is complete

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let grids = document.querySelectorAll("[data-cell]");
+grids.forEach(cell => {
+    cell.addEventListener('click', handleClick, {once: true})
+})
 
 let currentPiece = "X"
 
@@ -16,17 +19,94 @@ function placeMark(Cell) {
     play.innerText = nextPiece() 
 }
  
+function Win () {
+    let selectText = document.querySelector(`[data-winning-message-text]`)
+    let ChooseWin = currentPiece + ' WINS' 
+    selectText.innerText = ChooseWin;
+   let Winnings = document.getElementById("winnerMessage")
+   Winnings.classList.add('show');
+//    return document.getElementsByClassName('#show')
+}
 
-grids.forEach(cell => {
-    cell.addEventListener('click', handleClick, {once: true})
-})
+function Draw () {
+    let SelectText = document.querySelector(`[data-draw-message-text]`)
+    let chooseDraw = 'Its a Draw' 
+    SelectText.innerText = chooseDraw;
+    let Draws = document.getElementById("DrawMessage")
+    Draws.classList.add('draw');
+}
+
 /** @param e {MouseEvent} */      
 function handleClick(e){
-    console.log('clicked')
     let Click = e.target.getAttribute('data-cell')
     placeMark(Click);
-    //place mark
+    let PlayerWon = checkWin()
+    if (PlayerWon) {
+        Win()
+    }
+     else if (checkDraw()){
+        Draw()
+     }
+}
+
+let Winning_Combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+function checkWin() {
+    for (combination of Winning_Combinations){ 
+       let CheckWin = combination.map( index => {
+            return grids[index].innerText
+        })
+        if(CheckWin[0] == CheckWin[1] && CheckWin[2]== CheckWin[1]) {
+            return CheckWin[0]
+        }
+        console.log(CheckWin)
+    }
+}
+function checkDraw() {
+    for (const element of grids) { if (element.innerText == '')
+          return false
+        }
+       return true;
+ }
+
+
+
+
+// { 
+//     return  ! grids.any(x => x.innerText == "")
+// }
+
+
+
+
+
+    //    for (const element of grids) { if (element.innerText == '')
+    //      return false
+    //    }
+    //    return true;
+    // }
+
+let restartButton = document.getElementById("restartButton")
+restartButton.addEventListener('click', Restart);
+
+let restartButton2 = document.getElementById("restartButton2")
+restartButton2.addEventListener('click', Restart);
+
+function Restart() {
+    window.location.reload();
+}
+
+
+//place mark
     //check for win
     //check for draw
     //swicth turns
-}
